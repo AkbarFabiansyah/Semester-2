12d39d5418da590b018ae1e92eef1c5d084e0219
AkbarFabiansyah <realakbarofc@gmail.com>
2026-03-02 09:35:36 +0700

    push

--- pythop.py
+++ pythop.py
@@ -1,3 +1,5 @@
-print((2**4, (2*4.), (2*4)))
-print((-2/4), (2/4), (2//4), (-2//4))
-print((2%-4), (2%4), (2**3**2)
+# coba beberapa nilai x
+for x in [0, 1, -1]:
+    x = float(x)
+    y = 3*x**3 - 2*x**2 + 3*x - 1
+    print("x =", x, "-> y =", y)
