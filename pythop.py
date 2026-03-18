6e6268c001ea8cb1eb74c08aa1b858e0da218307
AkbarFabiansyah <realakbarofc@gmail.com>
2026-03-18 22:47:58 +0700

    cek

--- pythop.py
+++ pythop.py
@@ -1,5 +1,11 @@
-# coba beberapa nilai x
-for x in [0, 1, -1]:
-    x = float(x)
-    y = 3*x**3 - 2*x**2 + 3*x - 1
-    print("x =", x, "-> y =", y)
+x = 4
+y = 1
+
+a = x & y
+b = x | y
+c = ~x
+d = x ^ 5
+e = x >> 2
+f = x << 2
+
+print(a, b, c, d, e, f)
