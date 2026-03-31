c7b18cd0ae4b7cd2f4e10259806ab59109f2b774
AkbarFabiansyah <realakbarofc@gmail.com>
2026-03-31 10:34:25 +0700

    pertemuan 1

---
+++ Materi Kuliah/Alpro 2/Tugas/Pertemuan2.py
@@ -0,0 +1,77 @@
+#Soal 1
+print("Literal Pada Python")
+print(3)
+print(3.14)
+print("nama saya Akbar")
+print(True)
+print("/n/n")
+
+#Soal 2
+print("integer")
+print(111111)
+print(-11111)
+
+#soal 3
+print(0o17)
+print(0x1A)
+
+#soal 4
+print(0.4)
+print(4.0)
+
+#soal 5
+print("Tugas AlPro Pertemuan 2")
+
+#soal 6
+print(True)
+print(False)
+
+#soal 7
+print(2 ** 3)
+print(2 ** 3.0)
+print(2.0 ** 3)
+print(2.0 ** 3.0)
+
+#soal 8
+print(4 * 3)
+print(4 * 3.0)
+print(4.0 * 3)
+print(4.0 * 3.0)
+
+#soal 9
+print(8 / 2)
+print(8 / 2.0)
+print(8.0 / 2)
+print(8.0 / 2.0)
+
+#soal 10
+print(9 // 2)
+print(9 // 2.0)
+print(9.0 // 2)
+print(9.0 // 2.0)
+
+#soal 11
+print(9 % 2)
+print(9 % 2.0)
+print(9.0 % 2)
+print(9.0 % 2.0)
+
+#soal 12
+print(+5)
+print(-5)
+print(not True)
+print(4 + 3)
+print(4 - 3)
+print(4 * 3)
+print(8 / 2)
+
+#soal 13
+x = 10
+y = 5
+print((x + y) * 2)
+
+#Soal 14
+print((2**4, (2*4.), (2*4)))
+print((-2/4), (2/4), (2//4), (-2//4))
+print((2%-4), (2%4), (2**3**2))
+
