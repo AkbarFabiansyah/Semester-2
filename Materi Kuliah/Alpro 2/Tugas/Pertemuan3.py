5832809c81c7d777ce2ca144d89e4c77be350ca9
AkbarFabiansyah <realakbarofc@gmail.com>
2026-03-31 10:40:18 +0700

    Pertemuan 3

---
+++ Materi Kuliah/Alpro 2/Tugas/Pertemuan3.py
@@ -0,0 +1,73 @@
+#soal 1
+var = 3
+nilai = 90
+nama_mahasiswa = 'Akbar'
+print (var)
+print (nilai)
+print (nama_mahasiswa)
+print (var, nilai, nama_mahasiswa)
+
+#soal 2
+Nama = 'Budi'
+print(Nama)
+
+#soal 3
+umur = 18
+print(umur)
+umur = 19
+print(umur)
+
+#soal 4
+Tabungan = 100000
+print(Tabungan)
+Tabungan = 150000
+print(Tabungan)
+
+#soal 5
+nilai = 10
+nilai += 5 # sama dengan nilai = nilai + 5
+print(nilai)
+nilai = 10
+nilai -= 3 # sama dengan nilai = nilai - 3
+print(nilai)
+nilai = 10
+nilai *= 2 # sama dengan nilai = nilai * 2
+print(nilai)
+nilai = 10
+nilai /= 2 # sama dengan nilai = nilai / 2
+print(nilai)
+nilai = 10
+nilai %= 3 # sama dengan nilai = nilai % 3
+print(nilai)
+
+#soal 6
+apel_punya = 5
+apel_beli = 3
+total_apel = apel_punya + apel_beli
+print("Jumlah apel saya sekarang:", total_apel)
+
+#soal 7
+Ayu = 50000
+Bagus = 75000
+Citra = 60000
+print(Ayu, Bagus, Citra)
+jumlahTabungan = Ayu + Bagus + Citra
+print("Jumlah Tabungan adalah", jumlahTabungan)
+
+#soal 8
+kilometers = 12.25
+miles = 7.38
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2),
+"miles")
+
+#soal 9
+
+# coba beberapa nilai x
+for x in [0, 1, -1]:
+    x = float(x)
+    y = 3*x**3 - 2*x**2 + 3*x - 1
+    print("x =", x, "-> y =", y)
+
